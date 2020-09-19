2199de4199aa82007d747d5ae8b8016c7320cba1
MH2033 <mh.akbari@yahoo.com>
2020-09-19 14:32:02 +0430

    Enable WiFi access point for radio comunications

---
+++ include/wifi_comunication.h
@@ -0,0 +1,6 @@
+#ifndef WIFI_COMUNICATION_H
+#define WIFI_COMUNICATION_H
+
+void wifi_init_softap(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,17 +2,32 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "esp_attr.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
 
 #include "motor_controll.h"
+#include "wifi_comunication.h"
 
 /*A queue for storing power factors for each motor in order to balance the quadcopter*/
 QueueHandle_t power_factor_q = NULL;
 QueueHandle_t motor_speed_q = NULL;
 
 void app_main() {
-    /**/
+    /*Creating queues for motor control*/
     power_factor_q = xQueueCreate(2, sizeof(struct motor_data));
     motor_speed_q = xQueueCreate(2, sizeof(struct motor_data));
 
+    /*Starting motor control task*/
     xTaskCreate(motor_control, "Motor Controll Task", 2048, NULL, tskIDLE_PRIORITY + 2, NULL);
+
+    /*Initializing WiFi comunication*/
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    wifi_init_softap();
 }
