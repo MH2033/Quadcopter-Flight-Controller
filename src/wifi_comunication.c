019f32c54b8cb989eb0989395891985d947c5a0d
MH2033 <mh.akbari@yahoo.com>
2020-09-19 15:46:55 +0430

    Set defualt address for gateway

--- src/wifi_comunication.c
+++ src/wifi_comunication.c
@@ -32,8 +32,16 @@ void wifi_init_softap(void)
 {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
+    esp_netif_t* wifiAP = esp_netif_create_default_wifi_ap();
 
+    /*Setting gateway default IP Address*/
+    esp_netif_ip_info_t ipInfo;
+    IP4_ADDR(&ipInfo.ip, 192,168,10,1);
+	IP4_ADDR(&ipInfo.gw, 192,168,10,1);
+	IP4_ADDR(&ipInfo.netmask, 255,255,255,0);
+	esp_netif_dhcps_stop(wifiAP);
+	esp_netif_set_ip_info(wifiAP, &ipInfo);
+	esp_netif_dhcps_start(wifiAP);
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
